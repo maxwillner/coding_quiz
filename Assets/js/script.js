b8e48523b8d3546bd78227304a5408f2a6eb2197
Frankenstein <88062514+maxwillner@users.noreply.github.com>
2021-09-05 14:24:54 -0400

    added timer functionality; updated css formatting

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,3 +1,31 @@
-var timerEl = document.getElementById("countdown")
-var mainEl = document.getElementById("main")
-var startBtn = document.getElementById("start")
+// global variables
+var timerEl = document.getElementById("countdown");
+var mainEl = document.getElementById("main");
+var startBtn = document.getElementById("start");
+
+var questionsEl = document.getElementById("questions");
+
+// timer that counts down from 5 minutes
+function countdown() {
+    var timeLeft = 300;
+
+    //setInterval() method
+    var timeInterval = setInterval(function(){
+        if (timeLeft > 1) {
+            timerEl.textContent = timeLeft + " seconds remaining";
+            timeLeft --;
+        }
+        else if (timeLeft === 1) {
+            timerEl.textContent = timeLeft + " second remaining";
+            timeLeft --;
+        }
+        else {
+            timerEl.textContent = "";
+
+        // clearInterval
+        clearInterval(timeInterval);
+        }
+    }, 1000);
+};
+
+startBtn.onclick = countdown;
