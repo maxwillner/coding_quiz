76f9fc6202515325b3cd91dce7fa1924e77c7968
Frankenstein <88062514+maxwillner@users.noreply.github.com>
2021-09-05 22:33:37 -0400

    added question array

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,8 +2,50 @@
 var timerEl = document.getElementById("countdown");
 var mainEl = document.getElementById("main");
 var startBtn = document.getElementById("start");
+var questionEl = document.getElementById("question-text")
 
-var questionsEl = document.getElementById("questions");
+// questions array
+var questions = [
+    {
+        question: "Commonly used data types DO NOT include:",
+        incorrect: "Strings",
+        incorrect: "booleans",
+        correct: "alerts",
+        incorrect: "numbers"
+    },
+    {
+        question: "The condition in an if/else statement is enclosed with __________.",
+        incorrect: "quotes",
+        incorrect: "curly brackets", 
+        correct: "parenthesis",
+        incorrect: "square brackets"
+    },
+    {
+        question: "Arrays in JavaScript cana be used to store __________.",
+        incorrect: "numbers and strings",
+        incorrect: "other arrays",
+        incorrect: "booleans",
+        correct: "all of the above"
+    },
+    {
+        question: "String alues must be enclosed within __________ when being assigned to variables.",
+        incorrect: "commas",
+        incorrect: "curly brackets",
+        correct: "quotes",
+        incorrect: "parenthesis"
+    },
+    {
+        question: "A useful tool used during development and debugging for printing content to the debugger is:",
+        incorrect: "JavaScript",
+        incorrect: "terminal/bash",
+        incorrect: "for loops",
+        correct: "console.log"
+    }
+];
+
+// starting score of 0
+var score = 0;
+
 
 // timer that counts down from 5 minutes
 function countdown() {
@@ -27,5 +69,27 @@ function countdown() {
         }
     }, 1000);
 };
+
+//stuck start!!
+
+// questions pushed from array into questions-section
+function createQuestion () {
+    for (var i = 0; i < questions.length; i++) {
+        // dynamically create questions in HTML??
+
+
+        // compare answer??
+        if ((answer === correct)) {
+            score++
+        }
+        else {
+            ((answer === incorrect))
+            timeLeft = timeLeft - 25
+        };
+    }       
+};
+
+
+
 
 startBtn.onclick = countdown;
